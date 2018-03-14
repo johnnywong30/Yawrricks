45098194bae4b85092bcbd4d5f140974ed5e01ae
jwongUlti25 <jwong25@stuy.edu>
2018-03-14 00:30:24 -0400

    edited name stuff

--- QuickSort.java
+++ QuickSort.java
@@ -1,32 +1,32 @@
-//Zane Wang
-//APCS2 pd1
-//HW18 -- So So Quick
-//2018-03-13t
-  
+// Yawrricks - Johnny Wong, Richard Wong, and Zane Wang
+// APCS pd1
+// L01 -- What Does the Data Say?
+// 2018-03-14
+
 /*****************************************************
  * class QuickSort
  * Implements quicksort algo to sort an array of ints in place
  *
  * 1. Summary of QuickSort algorithm:
  * QSort(arr): calls partition to partition each "half" of the array until the
- * array has been partitioned to sections of length one. It will partition an 
- * array and then recursively partition the part of the array that is greater 
+ * array has been partitioned to sections of length one. It will partition an
+ * array and then recursively partition the part of the array that is greater
  * than and the part that is less than the pivot.
  *
- * 2a. Worst pivot choice and associated runtime: 
+ * 2a. Worst pivot choice and associated runtime:
  * The chosen pivot is the largest number because this results in going through
- * the whole array and calling partition, each time decreasing the size of the 
+ * the whole array and calling partition, each time decreasing the size of the
  * partitioned section by only 1, since the largest number would be by itself.
  * The runtime in this case would be O(n * n - 1)  or O(n^2).
  *
  * 2b. Best pivot choice and associated runtime:
  * The best case would be if the pivot was consistently the median because then
  * you can keep dividing the partitioned segment of the array by half, so there
- * would be a total of logn calls. The runtime in this case is O(n * logn) or 
+ * would be a total of logn calls. The runtime in this case is O(n * logn) or
  * O(nlogn).
  *
  * 3. Approach to handling duplicate values in array:
- * I changed the partition so that numbers less than or equal to would be sent
+ * We changed the partition so that numbers less than or equal to would be sent
  * to the left side, this was so that they would be able to clump together.
  *
  *****************************************************/
@@ -41,7 +41,7 @@ public class QuickSort
 	o[y] = tmp;
     }
 
-    //print input array 
+    //print input array
     public static void printArr( int[] a ) {
 	for ( int o : a )
 	    System.out.print( o + " " );
@@ -75,7 +75,7 @@ public class QuickSort
      * @param d -- array of ints to be sorted in place
      *****************************************************/
     public static void qsort( int[] d )
-    { 
+    {
 	qsortH(d, 0, d.length - 1);
     }
 
@@ -116,7 +116,7 @@ public class QuickSort
 	System.out.println("\narr1 init'd to: " );
 	printArr(arr1);
 
-	qsort( arr1 );	
+	qsort( arr1 );
 	System.out.println("arr1 after qsort: " );
 	printArr(arr1);
 
@@ -124,7 +124,7 @@ public class QuickSort
 	int[] arrN = new int[10];
 	for( int i = 0; i < arrN.length; i++ )
 	arrN[i] = i;
-       
+
 	System.out.println("\narrN init'd to: " );
 	printArr(arrN);
 
@@ -135,7 +135,7 @@ public class QuickSort
 	qsort( arrN );
 	System.out.println("arrN after sort: " );
 	printArr(arrN);
-	/*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y) 
+	/*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y)
 	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 
@@ -145,7 +145,7 @@ public class QuickSort
 	System.out.println("\narr2 init'd to: " );
 	printArr(arr2);
 
-	qsort( arr2 );	
+	qsort( arr2 );
 	System.out.println("arr2 after qsort: " );
 	printArr(arr2);
 
@@ -154,7 +154,7 @@ public class QuickSort
 	int[] arrMatey = new int[20];
 	for( int i = 0; i < arrMatey.length; i++ )
 	arrMatey[i] = (int)( 48 * Math.random() );
-       
+
 	System.out.println("\narrMatey init'd to: " );
 	printArr(arrMatey);
 
@@ -165,7 +165,7 @@ public class QuickSort
 	qsort( arrMatey );
 	System.out.println("arrMatey after sort: " );
 	printArr(arrMatey);
-	/*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y) 
+	/*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y)
 	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
     }//end main
